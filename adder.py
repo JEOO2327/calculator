7076aea2d4b325c28244801bec814275d8bb6dc2
한재훈 <1gkswogns@naver.com>
2016-04-29 16:50:59 +0900

    두번쨰

--- adder.py
+++ adder.py
@@ -1,8 +1,8 @@
-print('Input 1st number:')
-a=int(input())
-print('Input 2nd number:')
-b=int(input())
-c=a+b
-print('a+b')
-print(c)
 
+a=int(input("Input 1st number:"))
+b=int(input("Input 2nd number:" ))
+print(a,'+',b,'=',a+b)
+print(a,'-',b,'=',a-b)
+print(a,'*',b,'=',a*b)
+if b != 0;
+  print(a'/'b'='a/b)
